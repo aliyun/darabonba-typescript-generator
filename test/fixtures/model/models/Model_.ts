ec1dbb91f41b1b3fe6fafa8983633abd89beaade
peze <954152927@qq.com>
2025-05-13 10:27:00 +0800

    fix the model keyword

---
+++ test/fixtures/model/models/Model_.ts
@@ -0,0 +1,28 @@
+// This file is auto-generated, don't edit it
+import * as $dara from '@darabonba/typescript';
+
+
+export class Model extends $dara.Model {
+  stringfield: string;
+  static names(): { [key: string]: string } {
+    return {
+      stringfield: 'stringfield',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      stringfield: 'string',
+    };
+  }
+
+  validate() {
+    $dara.Model.validateRequired("stringfield", this.stringfield);
+    super.validate();
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
--- test/fixtures/model/models/model.ts
+++ test/fixtures/model/models/model.ts
@@ -2,5 +2,6 @@ export { MSubM } from './MsubM';
 export { MyModelSubmodel } from './MyModelSubmodel';
 export { MyModelSubarraymodel } from './MyModelSubarraymodel';
 export { M } from './M';
+export { Model } from './Model_';
 export { MyModel } from './MyModel';
 export { INT } from './Int';
